2c1f0273faf5e16ff32312f63c5b93ca01598301
James Fisher <jfisher396@hotmail.com>
2021-08-19 07:52:56 -0500

    update route created

--- controllers/fish.js
+++ controllers/fish.js
@@ -1,45 +1,73 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../models');
+const db = require("../models");
 
 // All routes start with /api/fishes
 
 // Route to get all fish in DB
-router.get('/', (req,res) => {
-    db.Fish.findAll().then((fishes) => {
-        res.json(fishes)
-    }).catch(err => {
-        console.log(err);
-        res.status(500).end()
+router.get("/", (req, res) => {
+  db.Fish.findAll()
+    .then((fishes) => {
+      res.json(fishes);
     })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
 });
 
 // Route to add a fish
-router.post('/', (req,res) => {
-    db.Fish.create({
-        name: req.body.name,
-        width: req.body.width,
-        color: req.body.color
-    }).then((newFish) => {
-        res.json(newFish);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).end()
+router.post("/", (req, res) => {
+  db.Fish.create({
+    name: req.body.name,
+    width: req.body.width,
+    color: req.body.color,
+  })
+    .then((newFish) => {
+      res.json(newFish);
     })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
 });
 
 // Route to delete fish
-router.delete('/:id', (req,res) => {
-    db.Fish.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then((deletedFish) => {
-        res.json(deletedFish)
-    }).catch(err => {
-        console.log(err);
-        res.status(500).end()
+router.delete("/:id", (req, res) => {
+  db.Fish.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedFish) => {
+      res.json(deletedFish);
     })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+
+// Route to update fish
+
+router.put("/:id", (req, res) => {
+  db.Fish.update(
+    {
+      width: req.body.width,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedFish) => {
+      res.json(updatedFish);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
 
 module.exports = router;
