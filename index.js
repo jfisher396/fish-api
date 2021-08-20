d35b49f319257356c1fba58791f0b7c94e3f4b56
James Fisher <jfisher396@hotmail.com>
2021-08-20 09:01:01 -0500

    installed cors npm package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors")
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -8,6 +9,11 @@ const db = require('./models');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true
+}))
 
 app.use('/', allRoutes);
 
