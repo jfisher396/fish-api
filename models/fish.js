3e161678867115d9a5b4a9aac73bc0ac13e9ed92
James Fisher <jfisher396@hotmail.com>
2021-08-23 17:42:51 -0500

    updated fish model

--- models/fish.js
+++ models/fish.js
@@ -1,8 +1,8 @@
 module.exports = function (sequelize, DataTypes) {
   const Fish = sequelize.define("Fish", {
-    name: DataTypes.STRING,
     width: DataTypes.INTEGER,
-    color: DataTypes.STRING
+    color1: DataTypes.STRING,
+    color2: DataTypes.STRING
   });
 
   return Fish;
