6a739a80ab49284a931590563bf0ef18c0fa347d
James Fisher <jfisher396@hotmail.com>
2021-09-02 14:06:08 -0500

    login route created

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const db = require("../models");
 const bcrypt = require("bcrypt");
 
+// All routes start with /api/users
+
 router.post("/signup", (req, res) => {
   db.User.create({
     name: req.body.name,
@@ -11,6 +13,29 @@ router.post("/signup", (req, res) => {
   })
     .then((newUser) => {
       res.json(newUser);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+
+router.post("/login", (req, res) => {
+  db.User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  })
+    .then((user) => {
+      if (!user) {
+        res.status(404).send("No user found with that email");
+      } else {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          res.send(`You are logged in under ${user.email}`);
+        } else {
+          res.status(401).send("Incorrect password");
+        }
+      }
     })
     .catch((err) => {
       console.log(err);
